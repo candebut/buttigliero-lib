d4fb00486056b215a7b855528502397fd2f11474
Candela Buttigliero <candelabuttigliero@MacBook-Pro-de-Candela.local>
2023-05-16 14:26:59 +0200

    button working

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,67 +1,13 @@
 import React from "react";
-import { useEffect, useRef, useState } from 'react';
-import propTypes from 'prop-types';
 import './Button.scss';
 
-function Button({
-    type = 'button',
-    variant = 'primary',
-    size = 'm',
-    weight = '500',
-    options = [],
-    children,
-    disabled,
-    ...props
-}) {
-    const [open, setOpen] = useState(false);
-    const drop = useRef(null);
-
-    useEffect(() => {
-        document.addEventListener('mousedown', handlePageClick);
-        return () => {
-            document.removeEventListener('mousedown', handlePageClick);
-        };
-    });
-
-    const handlePageClick = (e) => {
-        if (drop && !drop?.current?.contains(e.target) && open) {
-            setOpen(false);
-        }
-    };
-
-    const onClick = () => {
-        if (props.onClick) {
-            props.onClick();
-        }
-    };
-
-
-    return (
-        <div className="button-wrapper" ref={drop}>
-            <button
-                className={`button-${variant}`}
-                type={type}
-                onClick={onClick}
-                weight={weight}
-                disabled={disabled}
-                size={size}
-      
-                {...props}
-            >
-                {children}
-     
-            </button>
- 
-        </div>
-    );
+export interface ButtonProps {
+  label: string;
+  variant: string;
 }
 
-Button.propTypes = {
-    type: propTypes.string,
-    variant: propTypes.string,
-    size: propTypes.oneOf(['s', 'm']),
-    children: propTypes.oneOfType([propTypes.node, propTypes.string]),
-    disabled: propTypes.bool,
+const Button = ({label, variant = 'primary'}: ButtonProps) => {
+  return <div className='button-wrapper'><button className={`button-component__${variant}`}>{label}</button></div>;
 };
 
 export default Button;
