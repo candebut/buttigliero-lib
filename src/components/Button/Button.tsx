a3cf63e68cde2e7e59ac79d5e5c2406ae09be4ce
Candela Buttigliero <candelabuttigliero@MacBook-Pro-de-Candela.local>
2023-05-16 14:16:08 +0200

    adding styles to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,67 @@
 import React from "react";
+import { useEffect, useRef, useState } from 'react';
+import propTypes from 'prop-types';
+import './Button.scss';
 
-export interface ButtonProps {
-  label: string;
+function Button({
+    type = 'button',
+    variant = 'primary',
+    size = 'm',
+    weight = '500',
+    options = [],
+    children,
+    disabled,
+    ...props
+}) {
+    const [open, setOpen] = useState(false);
+    const drop = useRef(null);
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handlePageClick);
+        return () => {
+            document.removeEventListener('mousedown', handlePageClick);
+        };
+    });
+
+    const handlePageClick = (e) => {
+        if (drop && !drop?.current?.contains(e.target) && open) {
+            setOpen(false);
+        }
+    };
+
+    const onClick = () => {
+        if (props.onClick) {
+            props.onClick();
+        }
+    };
+
+
+    return (
+        <div className="button-wrapper" ref={drop}>
+            <button
+                className={`button-${variant}`}
+                type={type}
+                onClick={onClick}
+                weight={weight}
+                disabled={disabled}
+                size={size}
+      
+                {...props}
+            >
+                {children}
+     
+            </button>
+ 
+        </div>
+    );
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+Button.propTypes = {
+    type: propTypes.string,
+    variant: propTypes.string,
+    size: propTypes.oneOf(['s', 'm']),
+    children: propTypes.oneOfType([propTypes.node, propTypes.string]),
+    disabled: propTypes.bool,
 };
 
 export default Button;
