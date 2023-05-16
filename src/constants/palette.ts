5680d7bc145e3652ef4c2292e8753095f131d003
Candela Buttigliero <candelabuttigliero@MacBook-Pro-de-Candela.local>
2023-05-16 13:30:14 +0200

    updating dependencies, adding sass and began working on Button component

---
+++ src/constants/palette.ts
@@ -0,0 +1,44 @@
+export const PRIMARY_TURQOISE = '#4CCFE2';
+export const PRIMARY_TURQOISE_HOVER = '#40BED7';
+export const PRIMARY_PURPLE = '#93A2DD';
+export const PRIMARY_PURPLE_HOVER = '#8B99CF';
+export const PRIMARY_BLUE = '#5D78FF';
+export const PRIMARY_BLUE_HOVER = '#586BB5';
+
+// LIGHT COLORS
+export const LIGHT_TURQOISE = '#DFF9FD';
+export const LIGHT_ORANGE = '#FFECC3';
+export const LIGHT_RED = '#FDDFE8';
+export const LIGHT_GREEN = '#DFFDE6';
+export const LIGHT_GRASS_GREEN = '#EEFDDF';
+export const LIGHT_GREY = '#EBEDF2';
+export const LIGHT_ICE_BLUE = '#F5FAFF';
+export const LIGHT_CHARCOAL_GREY = '#F0F1F5';
+export const LIGHT_BLUE_GREY = '#EBF1F6';
+
+// VIVID COLORS
+export const ORANGE = '#F7941D';
+export const RED = '#FF3061';
+export const GREEN = '#99BE6A';
+export const YELLOW = '#FFB822';
+
+// MID/DARK COLORS
+export const ALMOST_BLACK = '#212529';
+export const DARK_GREY = '#80849B';
+export const MID_GREY = '#B2B9CD';
+export const DARK_BLUE = '#364C8D';
+export const DARK_GREEN = '#27B34B';
+export const DARK_TURQOISE = '#028698';
+
+// NEUTRAL
+export const WHITE = '#ffffff';
+export const BLACK = '#000000';
+
+// NOT NAMED YET
+export const LIGHTER_BLUE = '#A2C0DF';
+export const VERY_LIGHT_GREY = '#fafcff';
+export const LIGHTER_GREY = '#b1b3ba';
+export const LIGHTEST_GREY = '#f4f5f8';
+export const DARKER_GREY = '#9093A7';
+export const DARKEST_GREY = '#575962';
+export const ALTERNATIVE_BLUE = '#387FC9';
